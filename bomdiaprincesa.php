ab62d2d34c6abe28d732362fbb83cf2f15b2f0c3
ALUNO SESI MORENO <aluno.sesi@SISTEMAFIEPE.LOCAL>
2024-05-14 14:06:49 -0300

    novos projetos

---
+++ bomdiaprincesa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 style='color:red'>Calculo do amor: Aquiles e Elizabeth ❤️❤️❤️ <h1>
+    <img src="img/download (5).jpg" alt=""><br>
+</body>
+<?php
+
+$pessoa1="100";
+$pessoa2="100";
+$amor= ($pessoa1+$pessoa2)/2;
+
+echo "Essa é a porcentagem de o amor entre eles dar certo: $amor%";
+
+if ($amor<"60"){
+    echo"<br>";
+    echo"<br>";
+ echo "Está quase mano 🙁";
+}elseif($amor>"60"){
+    echo"<br>";
+    echo"<br>";
+    echo "O Amor está acontecendo ❤️";
+}
+
+?>
+</html>
---
+++ media.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cálculo da média dos alunos do 3 A do SESI Moreno</h1>
+    <?php
+    
+    $n1 = 9;
+    $n2 = 6;
+    $n3 = 7;
+    $total = ($n1+$n2+$n3)/3;
+
+    echo "A nota é essa rapazes $total";
+    
+
+    ?>
+</body>
+</html>
