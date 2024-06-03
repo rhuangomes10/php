e26011ef4d89d837ac0b3cbf141c982e825d4bb7
ALUNO SESI MORENO <aluno.sesi@SISTEMAFIEPE.LOCAL>
2024-06-03 16:06:24 -0300

    pista

---
+++ pista.php
@@ -0,0 +1,14 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+   require_once('praticapoo.php');
+   $meu_carro = new Carro;
+   $meu_carro->ligar();
+    ?>
+</body>
+</html>
